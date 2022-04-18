14807165456fa62db30cd93fd933dd855dedeb6f
Bethsyf <bethsyfalcon@gmail.com>
2022-04-18 05:54:21 -0500

    ruta img

--- scripts/detail.js
+++ scripts/detail.js
@@ -6,7 +6,7 @@ const getLocalStorage = () => {
 
   detail.innerHTML += `
     <header>
-      <a class="btnBack" href="./home.html"><img src="../images/back.png" alt="back"></a>
+      <a class="btnBack" href="./home.html"><img src="./images/back.png" alt="back"></a>
       <img src="${imagen}" class="imgPrincipal" alt="imagen">
     </header>
     <main id="detailsMain">
